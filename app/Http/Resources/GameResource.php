4dc902f7626690812fb00c57bd70dbad43389112
sergio <sergioans95@hotmail.com>
2021-11-08 20:21:16 -0300

    creando gameResource

---
+++ app/Http/Resources/GameResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+use Carbon\Carbon;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GameResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'url' => $this->url,
+            'url_image' => $this->url_image,
+            'status' => $this->status,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
