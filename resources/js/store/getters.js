97e8e1aaaa3e74373d967d35beb0cb15933023c1
sergio <sergioans95@hotmail.com>
2021-11-10 22:15:12 -0300

    creando store para juegos

---
+++ resources/js/store/getters.js
@@ -0,0 +1,10 @@
+let getters = {
+    games: state => {
+        return state.games
+    },
+    gameById: state => {
+        return state.gameById
+    }
+}
+
+export default getters
---
+++ resources/js/store/mutations.js
@@ -0,0 +1,21 @@
+var mutations = {
+    FETCH_GAMES(state, games) {
+        return state.games = games
+    },
+    CREATE_GAME(state, game) {
+        state.games.push(game)
+    },
+    DELETE_GAME(state, game) {
+        var index = state.games.findIndex(item => item.id === game)
+        state.games.splice(index, 1)
+    },
+    EDIT_GAME(state, game) {
+        var index = state.games.findIndex(item => item.id === game.id)
+        return index
+    },
+    GET_GAME(state, game) {
+        var index = state.games.find(item => item.id === game.id)
+        return state.gameById = index;
+    }
+}
+export default mutations
