cca59b12c50566f5ef8642c61b413d5bf1a96deb
sergio <sergioans95@hotmail.com>
2021-11-12 10:40:55 -0300

    usando async await

--- resources/js/store/actions.js
+++ resources/js/store/actions.js
@@ -1,52 +1,58 @@
 import axios from 'axios';
+import router from '../router';
 
-var apiGame = '/api/game'
+const apiGame = '/api/game'
 let actions = {
-    fetchGames({ commit }) {
-        axios.get(apiGame)
-            .then(res => {
-                commit('FETCH_GAMES', res.data)
-            }).catch(err => {
-                console.log(err)
-            })
-    },
-    createGame({ commit }, game) {
-        axios.post(apiGame, game)
-            .then(res => {
-                commit('CREATE_GAME', res.data)
-            }).catch(err => {
-                console.log(err)
-            })
+    async fetchGames({ commit }) {
+        try {
+            const res = await axios.get(apiGame)
+            commit('FETCH_GAMES', res.data)
 
+        } catch (error) {
+            console.log(error)
+        }
     },
-    editGame({ commit }, game) {
-        axios.put(`${apiGame}/${game.id}`, game)
-            .then(res => {
-                if (res.status == 200) {
-                    commit('EDIT_GAME', res.data)
-                }
-            }).catch(err => {
-                console.log(err)
-                console.log(err.response.data.errors)
-            })
+    async createGame({ commit }, game) {
+        try {
+            const res = await axios.post(apiGame, game)
+            if (res.status == 201) {
+                commit('CREATE_GAME', res.data)
+                router.push({ name: 'inicio' })
+            }
+        } catch (error) {
+            console.log(error)
+        }
     },
-    getGameById({ commit }, gameId) {
-        axios.get(`${apiGame}/${gameId}`)
-            .then(res => {
-                commit('GET_GAME', res.data)
-            }).catch(err => {
-                console.log(err)
-            })
+    async editGame({ commit }, game) {
+        try {
+            const res = await axios.put(`${apiGame}/${game.id}`, game)
+            if (res.status == 200) {
+                commit('EDIT_GAME', res.data)
+                router.push({ name: 'inicio' })
+            }
+        } catch (error) {
+            console.log(error)
+            console.log(error.response.data.errors)
+        }
+},
+    async getGameById({ commit }, gameId) {
+        try {
+            const res = await axios.get(`${apiGame}/${gameId}`)
+            commit('GET_GAME', res.data)
+        } catch (error) {
+            console.log(error)
+        }
     },
-    deleteGame({ commit }, game) {
-        axios.delete(`${apiGame}/${game}`)
-            .then(res => {
-                if (res.data.message === 'delete success')
-                    commit('DELETE_GAME', game)
-            }).catch(err => {
-                console.log(err)
-            })
+        async deleteGame({ commit }, game) {
+    try {
+        const res = await axios.delete(`${apiGame}/${game}`)
+        if (res.data.message === 'delete success') {
+            commit('DELETE_GAME', game);
+        }
+    } catch (error) {
+        console.log(error)
     }
+},
 }
 
 export default actions
--- resources/js/store/mutations.js
+++ resources/js/store/mutations.js
@@ -1,4 +1,4 @@
-var mutations = {
+const mutations = {
     FETCH_GAMES(state, games) {
         return state.games = games
     },
@@ -6,15 +6,15 @@ var mutations = {
         state.games.push(game)
     },
     DELETE_GAME(state, game) {
-        var index = state.games.findIndex(item => item.id === game)
+        const index = state.games.findIndex(item => item.id === game)
         state.games.splice(index, 1)
     },
     EDIT_GAME(state, game) {
-        var index = state.games.findIndex(item => item.id === game.id)
+        const index = state.games.findIndex(item => item.id === game.id)
         return index
     },
     GET_GAME(state, game) {
-        var index = state.games.find(item => item.id === game.id)
+        const index = state.games.find(item => item.id === game.id)
         return state.gameById = index;
     }
 }
