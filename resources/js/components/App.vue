6a985ff8dbbeda9c268af79f2afe9a5243d3efed
sergio <sergioans95@hotmail.com>
2021-11-20 19:26:57 -0300

    cambio menor

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col"><h1>juegos de azar</h1></div>
+      <div class="col"><h1>Juegos de azar</h1></div>
       <router-link to="/create" class="btn btn-success" tag="button">Añadir Juego</router-link>
     </div>
     <router-view></router-view>
