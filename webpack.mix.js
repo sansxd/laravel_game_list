5e519c6bccf6f9c08fadad9a8d5b8735298d1a53
sergio <sergioans95@hotmail.com>
2021-11-20 19:25:29 -0300

    añadiendo browserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
+    .browserSync('http://localhost:8000/')
     .disableSuccessNotifications();
 
